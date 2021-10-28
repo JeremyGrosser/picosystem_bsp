df711f015085c81193a544afab2ec06014d6e702
Jeremy Grosser <jeremy@synack.me>
2021-10-27 18:36:28 -0700

    DMA transfer one row of pixels at a time, double buffered

--- src/picosystem-screen.adb
+++ src/picosystem-screen.adb
@@ -8,6 +8,7 @@ with RP.Device;
 with RP.GPIO;
 with RP.SPI;
 with ST7789;
+with HAL.SPI;
 
 package body Picosystem.Screen is
 
@@ -21,7 +22,7 @@ package body Picosystem.Screen is
    procedure Initialize is
       use RP.GPIO;
 
-      SPI_Config : constant RP.SPI.SPI_Configuration :=
+      SPI_Config : RP.SPI.SPI_Configuration :=
          (Baud     => 8_000_000,
           Blocking => True,
           others   => <>);
@@ -41,10 +42,31 @@ package body Picosystem.Screen is
          RP.Device.Timer.Enable;
       end if;
       LCD.Initialize;
+
+      --  Reconfigure for 16-bit transfers
+      --  8.8.42 Write data for 16-bit/pixel (RGB-5-6-5-bit input), 65K-Colors, 3Ah=”05h”
+      SPI_Config.Data_Size := HAL.SPI.Data_Size_16b;
+      LCD_SPI.Configure (SPI_Config);
+
+      declare
+         use RP.DMA;
+      begin
+         RP.DMA.Configure
+            (Channel => DMA_Channel,
+             Config  =>
+               (Data_Size      => Transfer_16,
+                Increment_Read => True,
+                Trigger        => SPI0_TX,
+                others         => <>));
+      end;
    end Initialize;
 
    procedure Wait_VSync is
    begin
+      while RP.DMA.Busy (DMA_Channel) loop
+         null;
+      end loop;
+
       --  If already in vsync, wait for it to end
       while LCD_VSYNC.Get loop
          null;
@@ -55,5 +77,34 @@ package body Picosystem.Screen is
          null;
       end loop;
    end Wait_VSync;
+
+   procedure Swap_Buffers is
+      Tmp : Buffer_Index;
+   begin
+      Tmp := Reading;
+      Reading := Writing;
+      Writing := Tmp;
+   end Swap_Buffers;
+
+   procedure Write
+      (P : Pixels)
+   is
+   begin
+      Buffers (Writing) := P;
+
+      --  Wait for previous DMA transfer
+      while RP.DMA.Busy (DMA_Channel) loop
+         null;
+      end loop;
+
+      Swap_Buffers;
+
+      --  Start DMA transfer
+      RP.DMA.Start
+         (Channel => DMA_Channel,
+          From    => Buffers (Reading)'Address,
+          To      => LCD_SPI.FIFO_Address,
+          Count   => Buffers (Reading)'Length);
+   end Write;
 
 end Picosystem.Screen;
--- src/picosystem-screen.ads
+++ src/picosystem-screen.ads
@@ -3,6 +3,9 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
+with HAL; use HAL;
+with RP.DMA;
+
 package Picosystem.Screen is
 
    Width  : constant := 240;
@@ -10,5 +13,29 @@ package Picosystem.Screen is
 
    procedure Initialize;
    procedure Wait_VSync;
+
+   type Color is record
+      R : UInt5;
+      G : UInt6;
+      B : UInt5;
+   end record
+      with Pack, Size => 16;
+
+   --  Write one row of pixels at a time, double buffered.
+   type Pixels is array (1 .. Width) of Color;
+   procedure Write
+      (P : Pixels);
+
+private
+
+   DMA_Channel : constant RP.DMA.DMA_Channel_Id := 0;
+   DMA_Trigger : constant RP.DMA.DMA_Request_Trigger := RP.DMA.SPI0_TX;
+
+   type Buffer_Index is range 1 .. 2;
+   Buffers : array (Buffer_Index) of Pixels;
+   Reading : Buffer_Index := 1;
+   Writing : Buffer_Index := 2;
+
+   procedure Swap_Buffers;
 
 end Picosystem.Screen;
